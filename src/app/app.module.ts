8616dcaa790c4672e232bdf24abd9edacfdc634c
yash <you@example.com>
2018-07-02 17:01:11 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { AppheaderComponent } from './components/appheader/appheader.component';
+import { AppfooterComponent } from './components/appfooter/appfooter.component';
+import { AppmenuComponent } from './components/appmenu/appmenu.component';
+import { AppsettingComponent } from './components/appsetting/appsetting.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppheaderComponent,
+    AppfooterComponent,
+    AppmenuComponent,
+    AppsettingComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
